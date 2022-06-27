c9f393c171651bd168ea11797eece548b3e7463c
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-06-27 22:45:41 +0200

    Clean up js logic flow and add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,6 @@
 // Assignment code here: Module 3 Challenge
 
-// Capture password length choice and change object type to number type 
-
-let passwordLength = 8; // default value
-
-function getNewPWLength(getNewPWLength) {
-  passwordLength = Number.parseInt(getNewPWLength.value);
-  console.log(passwordLength);
-}
-
-// Capture click on input fields that include the word "option"
-document.getElementById("option-numbers").addEventListener("click", setLabelColor);
-document.getElementById("option-lowercase").addEventListener("click", setLabelColor);
-document.getElementById("option-uppercase").addEventListener("click", setLabelColor);
-document.getElementById("option-special").addEventListener("click", setLabelColor);
-
 // Set values for letters, numbers, characters for generic password generation
-
 let randomAll = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789[~!@#$%^&*()_+-=[]\{}|;:\'\",./<>?\]";
 let alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let alphaLower = "abcdefghijklmnopqrstuvwxyz";
@@ -25,25 +9,27 @@ let characters = "[~!@#$%^&*()_+-=[]\{}|;:\'\",./<>?\]";
 let include = false;
 let chars = [alphaUpper, alphaLower, choiceNumbers, characters];
 let randomString = randomAll;
-
+let passwordLength = 8; // default value
 
 // Verify that current input (this.id) is checked  by changing its label style color to green and return to default black when unchecked
 function setLabelColor() {
-  let selector = `[for=${this.id}]`
-  console.log(selector);
-  let label = document.querySelector(selector);
-  if (this.checked) {
-    label.style.color = "green";
-  } else {
-    label.style.color = "black";
+    let selector = `[for=${this.id}]`
+    console.log(selector);
+    let label = document.querySelector(selector);
+    if (this.checked) {
+      label.style.color = "green";
+    } else {
+      label.style.color = "black";
+    };
   };
-};
 
-// ! TODO use selector to change chars variable
+// Capture password length choice and change object type to number type 
+function getNewPWLength(getNewPWLength) {
+  passwordLength = Number.parseInt(getNewPWLength.value);
+  console.log(passwordLength);
+}
 
-
-// Create random strings per https://www.mediacollege.com/internet/javascript/number/random.html and test with single variable
-
+// Listen for button click and then generate random string
 function generatePassword() {
 	chars = randomAll;
 	let stringLength = passwordLength;
@@ -53,22 +39,27 @@ function generatePassword() {
 		randomString += chars.substring(rnum,rnum+1);
 	}
 
-// Place value of string in the text area
-  document.getElementById("password").value = randomString;
+    // Place value of string in the text area
+    document.getElementById("password").value = randomString;
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+    // Get references to the #generate element
+    var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    // Add event listener to generate button
+    generateBtn.addEventListener("click", writePassword);
 }
 
-// Write password to the #password input
+// Write random string to the #password input
 function writePassword() {
-  let password = randomString;
+    let password = randomString;
+    let passwordText = document.querySelector("#password");
+      passwordText.value = password;
+  }
   let passwordText = document.querySelector("#password");
+  console.log(passwordText.value);
 
-  passwordText.value = password;
-}
-let passwordText = document.querySelector("#password");
-console.log(passwordText.value);
+// Capture click on input fields that include the word "option"
+document.getElementById("option-numbers").addEventListener("click", setLabelColor);
+document.getElementById("option-lowercase").addEventListener("click", setLabelColor);
+document.getElementById("option-uppercase").addEventListener("click", setLabelColor);
+document.getElementById("option-special").addEventListener("click", setLabelColor);
