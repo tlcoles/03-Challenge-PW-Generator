00167c46d63f2111b2115fbcef63f58f6e206c0d
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-06-27 22:00:34 +0200

    Provide better messaging to user

--- README.md
+++ README.md
@@ -16,18 +16,18 @@ The resulting user interface must be responsive, clean, and polished.
 * Set query to set color for selection
 * Capture and pass through password length 
 
-
 ## Challenges
-* Changing style based on selector criterium
+* Changing style based on selector criteria
 * Passing user selection from a range input to output using js textContent method and  oninput call in html
 * Converting input object and converting it to a number using parseInt() 
-
-
-
+* Scope of values (not just in functions)
+* Targeting element for output with getElementById 
 
 ## Gratitude
 * Xandromus via Github for project starter code
-* Rahul R. via StackOverflow for answer to "HTML5 input type range show range value"
+* Rahul R. via StackOverflow for answer to "HTML5 input type range show range value" [oninput]
+* Programmer buds Morgan R. & Tiffany C. for stepping in when it all started looking like Greek to me
+* MediaCollege.com for "How to Create Random Numbers & Characters"
 
 ## Contact
 Tammi L. Coles  
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,74 @@
 // Assignment code here: Module 3 Challenge
 
 // Capture password length choice and change object type to number type 
+
 let passwordLength = 8; // default value
 
 function getNewPWLength(getNewPWLength) {
-  let x = Number.parseInt(getNewPWLength.value);
-  console.log (x);
-  console.log(typeof x);
+  passwordLength = Number.parseInt(getNewPWLength.value);
+  console.log(passwordLength);
 }
 
-
 // Capture click on input fields that include the word "option"
-
 document.getElementById("option-numbers").addEventListener("click", setLabelColor);
 document.getElementById("option-lowercase").addEventListener("click", setLabelColor);
 document.getElementById("option-uppercase").addEventListener("click", setLabelColor);
 document.getElementById("option-special").addEventListener("click", setLabelColor);
+
+// Set values for letters, numbers, characters for generic password generation
+
+let randomAll = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789[~!@#$%^&*()_+-=[]\{}|;:\'\",./<>?\]";
+let alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let alphaLower = "abcdefghijklmnopqrstuvwxyz";
+let choiceNumbers = "0123456789";
+let characters = "[~!@#$%^&*()_+-=[]\{}|;:\'\",./<>?\]";
+let include = false;
+let chars = [alphaUpper, alphaLower, choiceNumbers, characters];
+let randomString = randomAll;
+
 
 // Verify that current input (this.id) is checked  by changing its label style color to green and return to default black when unchecked
 function setLabelColor() {
   let selector = `[for=${this.id}]`
   console.log(selector);
   let label = document.querySelector(selector);
-
   if (this.checked) {
     label.style.color = "green";
-
   } else {
     label.style.color = "black";
   };
 };
 
-// Set values for letters, numbers, characters for generic password generation
+// ! TODO use selector to change chars variable
 
-let randomNumbers = "^[A-Za-z][A-Za-z0-9!@#$%^&*]*$";
-let alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let alphaLower = "abcdefghijklmnopqrstuvwxyz";
-let choiceNumbers = "0123456789";
-let characters = "[~!@#$%^&*()_+-=[]\{}|;:\'\",./<>?\]";
 
-// TODO if just alphaUpper
-// ? set chars equal to alphaUpper
-// ? set string_length to getPasswordLength.value
-// ! Just try some borrowed code in the meantime
-
-// Create random strings per https://www.mediacollege.com/internet/javascript/number/random.html
+// Create random strings per https://www.mediacollege.com/internet/javascript/number/random.html and test with single variable
 
 function generatePassword() {
-	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-	var stringLength = 40;
-	var randomString = '';
+	chars = randomAll;
+	let stringLength = passwordLength;
+	randomString = '';
 	for (var i=0; i<stringLength; i++) {
 		var rnum = Math.floor(Math.random() * chars.length);
 		randomString += chars.substring(rnum,rnum+1);
 	}
-	document.randform.randomfield.value = randomString;
-console.log(randomString);
 
+// Place value of string in the text area
+  document.getElementById("password").value = randomString;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-console.log(generateBtn);
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 }
 
-/*
 // Write password to the #password input
 function writePassword() {
-  let password = generatePassword();
+  let password = randomString;
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  console.log("passwordText");
 }
 let passwordText = document.querySelector("#password");
 console.log(passwordText.value);
-*/
